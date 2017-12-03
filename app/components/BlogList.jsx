a972ee8a7954d9c616b05ae0b3785bf497865450
Matt Thompson <matt34thompson@gmail.com>
2017-12-03 19:44:06 +0000

    Sorted getting data into BlogList

--- app/components/BlogList.jsx
+++ app/components/BlogList.jsx
@@ -1,7 +1,5 @@
 var React = require('react');
 
-
-// pull in data{}
 // describe the javascript to slice part of the content
 // in render() list the blogs to be displayed in order [0], [1], [n]
 
@@ -18,18 +16,11 @@ var BlogList = React.createClass({ //try not to use .createClass (deprecated)
   componentWillMount: function (blogs) {
     var that = this;
 
-    // neither of my WHILE loops seem to be working
-    // think it should be of the form... while (this thing is true) {then do this}. when it isn't true, stop.
-
     cosmic.getBlogs().then(function (blogs) {
-
-      while (that.state.blogs != undefined) {
-        that.setState({ // calling 'that' to use the 'this' keyword
-          blogs: blogs,
-          isLoading: false
-        });
-        console.log('ComponentWillMount: blogs should exist: ' + that.state.blogs)
-      }
+      that.setState({
+        blogs: blogs,
+        isLoading: false
+      });
     }, function (errorMessage) {
       that.setState({isLoading: false}),
       alert(errorMessage);
@@ -41,29 +32,31 @@ var BlogList = React.createClass({ //try not to use .createClass (deprecated)
     var {isLoading, blogs} = this.state;
 
     function renderBlogList () {
-      console.log('render: blogs should be undefined: ' + that.state.blogs);
-      while (blogs != undefined) {
-        console.log('render: blogs should now exist: ' + that.state.blogs);
+      if (isLoading) {
+        return <h5 className="text-center page-loading">Getting blogs...</h5>;
+      } else if (blogs) {
+        console.log('Blog object [0]: ' + blogs.objects[0].title);
+
+        var i = 0;
+        var highArrPosition = blogs.objects.length - 1;
+        console.log(highArrPosition);
+        //if (i=0, i<highArrPosition, i++) {
+          // do stuff to render each preview up to position highArrPosition
+        // }
+        var {title, created, content} = blogs.objects[i];
+        var author = that.state.blogs.objects[i].metadata.author.title;
+        var displayDate = created.charAt(8) + created.charAt(9) + '/' + created.charAt(5) + created.charAt(6) + '/' + created.charAt(0) + created.charAt(1) + created.charAt(2) + created.charAt(3);
+        var preview = content.substring(0, 150) + '... >>';
+
         return (
           <div>
-            <p>Blogs?</p>
+            <p>Blogs!</p>
+            <div>{preview}</div>
           </div>
         )
       }
     }
 
-
-    // var {isLoading, blogs} = this.state;
-    //
-    // var {title, created, content} = blogs.objects[0]; // might not need the 'objects' bit, just blogs[0]
-    // // var title = this.state.blogs.objects[0].title;
-    // // var author = this.state.blogs.objects[0].metadata.author.title;
-    // var author = 'mattTea';
-    // // var created = this.state.blogs.objects[0].created;
-    // // var displayDate = created.charAt(8) + created.charAt(9) + '/' + created.charAt(5) + created.charAt(6) + '/' + created.charAt(0) + created.charAt(1) + created.charAt(2) + created.charAt(3);
-    // var displayDate = 'today';
-    // // var content = this.state.blogs.objects[0].content;
-    // var preview = content.substring(0, 150) + '... >>';
     //
     // var blog9 = () => {
     //   return (
@@ -77,18 +70,6 @@ var BlogList = React.createClass({ //try not to use .createClass (deprecated)
     //       </div></a>
     //     </div>
     //   )
-    // }
-    //
-    // function renderBlogList () {
-    //   if (isLoading) {
-    //     return <h5 className="text-center page-loading">Getting blogs...</h5>;
-    //   } else if (blogs) {
-    //     return (
-    //       <div>
-    //         {blog9}
-    //       </div>
-    //     )
-    //   }
     // }
 
     return (
