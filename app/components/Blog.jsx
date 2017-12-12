0ec8c72693145ed33ef188db913e0c352247b899
Matt Thompson <matt34thompson@gmail.com>
2017-12-12 21:58:02 +0000

    Started basic Blog component

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
+import ReactHtmlParser from 'react-html-parser';
 
+var cosmic = require('cosmic');
 
 // I have the content of the blog object already pulled in and known in the BlogList component
 // * Can I use this and pass it to the Blog component so it renders the specific blog that is linked from?
@@ -7,29 +9,58 @@ var React = require('react');
 // * Prev and Next arrows will scroll through array
 // * Can I make the blog attributes (in BlogList) the props to pass into the Blog component somehow?
 
+//start with displaying blog at [0]
 
 var Blog = React.createClass({ //try not to use .createClass (deprecated)
   getInitialState: function () {
     return {
-      title: 'blogHoldingPage'
+      isLoading: true,
+      blogs: undefined
     }
   },
 
+  componentWillMount: function (blogs) {
+    var that = this;
+
+    cosmic.getBlogs().then(function (blogs) {
+      that.setState({
+        blogs: blogs,
+        isLoading: false
+      });
+    }, function (errorMessage) {
+      that.setState({isLoading: false}),
+      alert(errorMessage);
+    });
+  },
+
   render: function () {
-    var {title} = this.state;
+    var that = this;
+    var {isLoading, blogs} = this.state;
 
     function renderBlog () {
-      return (
-        <div>
-          <h5>{title}</h5>;
-        </div>
-      )
+      if (isLoading) {
+        return <h5 className="text-center page-loading">Getting latest blog...</h5>;
+      } else if (blogs) {
+        var {title, created, content} = blogs.objects[0];
+        var author = that.state.blogs.objects[0].metadata.author.title;
+        var displayDate = created.charAt(8) + created.charAt(9) + '/' + created.charAt(5) + created.charAt(6) + '/' + created.charAt(0) + created.charAt(1) + created.charAt(2) + created.charAt(3);
+        // also need blog image above
+
+        return (
+          <div>
+            <div className="title">{title}</div>
+            <div className="content">{ReactHtmlParser(content)}</div>
+            <div className="author">{author}</div>
+            <div className="created">{displayDate}</div>
+            <div className="clear"></div>
+          </div>
+        )
+      }
     }
 
     return (
-      <div>
-        {renderBlog}
-        <p>Blog component rendered</p>
+      <div className="container">
+        {renderBlog()}
       </div>
     );
   }
