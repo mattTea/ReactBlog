3b471a8e1b63b9407a88f2ae082ae0fdae99891c
Matt Thompson <matt34thompson@gmail.com>
2017-12-12 21:42:45 +0000

    BlogList component complete save for links to individual blogs

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -1,5 +1,13 @@
 var React = require('react');
 
+
+// I have the content of the blog object already pulled in and known in the BlogList component
+// * Can I use this and pass it to the Blog component so it renders the specific blog that is linked from?
+// * Blog will always render first time with blog at [0] in Home page
+// * Prev and Next arrows will scroll through array
+// * Can I make the blog attributes (in BlogList) the props to pass into the Blog component somehow?
+
+
 var Blog = React.createClass({ //try not to use .createClass (deprecated)
   getInitialState: function () {
     return {
--- app/components/BlogList.jsx
+++ app/components/BlogList.jsx
@@ -1,7 +1,6 @@
 var React = require('react');
-// import ReactHtmlParser from 'react-html-parser';
-// const json = require('json-loader!./file.json');
-// const json = require('./file.json');
+import ReactHtmlParser from 'react-html-parser';
+var {Link, IndexLink} = require('react-router');
 
 var cosmic = require('cosmic');
 
@@ -35,88 +34,48 @@ var BlogList = React.createClass({ //try not to use .createClass (deprecated)
       if (isLoading) {
         return <h5 className="text-center page-loading">Getting blogs...</h5>;
       } else if (blogs) {
-        console.log('Blog object [0]: ' + blogs.objects[0].title);
 
-        // var i = 0;
         var noOfBlogs = blogs.objects.length;
-        console.log(noOfBlogs);
 
         var blogItemsArray = [];
 
         for (var i = 0; i < noOfBlogs; i++) {
           var {title, created, content} = blogs.objects[i];
 
-          function decodeEntities(encodedString) {
-            var textArea = document.createElement('textarea');
-            textArea.innerHTML = encodedString;
-            return textArea.value;
-          }
-
-          var contentDecoded = decodeEntities(content);
-          // console.log('contentDecoded: ' + contentDecoded);
-
-          // CAN I PUSH THIS OBJECT TO A NEW ARRAY? Yes
-          // THEN RUN A MAP() OVER THE NEW ARRAY?
           var blogItem = {
             id: i,
             title: title,
             author: that.state.blogs.objects[i].metadata.author.title,
             displayDate: created.charAt(8) + created.charAt(9) + '/' + created.charAt(5) + created.charAt(6) + '/' + created.charAt(0) + created.charAt(1) + created.charAt(2) + created.charAt(3),
-            preview: contentDecoded.substring(0, 150) + '... >>'
+            preview: content.substring(0, 150) + '... >>'
           }
-
           blogItemsArray.push(blogItem);
-          console.log('blogItemsArray: ' + blogItemsArray);
-
-          // create & display separate div for each comment
-
-          // if (!document.getElementById('blogPreviewList')) {
-          //   var divElement = document.createElement('divElement');
-          //   divElement.id = 'blogPreviewList';
-          //   document.body.appendChild(divElement);
-          // }
-          //
-          // var div = document.createElement('div');
-          // document.body.appendChild(div);
-          // document.getElementById('blogPreviewList').appendChild(div);
-          // div.id = 'blogPreviewList' + i;
-          // $("#blogPreviewList" + i).append(blogItem);
-          // console.log('div.id: ' + div.id);
-
-          // NOW I HAVE THESE INDIVIDUAL DIVs, how push blogItem attributes and render each one?
-          //
-
         }
 
         const blogListItems = blogItemsArray.map((blogItemsArray, index) =>
           <li key={index}>
-            {blogItemsArray.title}
-            {blogItemsArray.preview}
-            {blogItemsArray.author}
-            {blogItemsArray.displayDate}
+            {/* try to make an array of href links so we can link to the blogs using below <a> tags */}
+            <Link to="/articles">
+              <div className="title">{blogItemsArray.title}</div>
+              <div className="preview">{ReactHtmlParser(blogItemsArray.preview)}</div>
+              <div className="author">{blogItemsArray.author}</div>
+              <div className="created">{blogItemsArray.displayDate}</div>
+              <div className="clear"></div>
+            </Link>
           </li>
         );
 
         return (
           <div id="blogPreviewList">
-            {/* I think I need to add the create div and append child if statement somewhere here */}
             <ul>{blogListItems}</ul>
-            {/* <a href="#"><div>
-              <div className="title">{blogItem.title}</div>
-              <div className="preview">{blogItem.preview}</div>
-              <div className="preview">{ReactHtmlParser(preview)}</div>
-              <div className="author">{blogItem.author}</div>
-              <div className="created">{blogItem.displayDate}</div>
-              <div className="clear"></div>
-              </div></a> */}
           </div>
         )
       }
     }
 
     return (
-      <div>
-        <h2 className="">the blogs</h2>
+      <div className="container">
+        <h2>the blogs...</h2>
         {renderBlogList()}
       </div>
     );
