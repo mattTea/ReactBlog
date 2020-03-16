baf067aefe7a2bb7ea9ba0879a72a4d4effca14d
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-03-16 09:16:26 +0000

    Revert changes and add only cosmic footer and package.json changes

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -10,6 +10,7 @@ var Footer = (props) => {
       <div id="footer">
         <div className="container">
           <p>mattTea | B.A(ng)</p>
+          <a href="https://app.cosmicjs.com/add-bucket?import_bucket=5884ea9522c70c1524000003"><img src="https://cdn.cosmicjs.com/51fe54d0-4f6e-11e9-9f32-8d001da69630-powered-by-cosmicjs.svg" /></a>
         </div>
       </div>
     </div>
